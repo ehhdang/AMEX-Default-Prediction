18b4b6c72043a2f5f4db3dc130ff13a67103c5c1
amrit <ap4.singh@gmail.com>
2022-11-06 16:56:44 -0500

    constants update to dataset

--- src/constants.py
+++ src/constants.py
@@ -1,17 +1,17 @@
 paths = {
 
-    'sample_data' : './data/train_gwm_100k.parquet', #First 100K rows of the total train data
+    'sample_data' : './dataset/train_gwm_100k.parquet', #First 100K rows of the total train data
 
     # Data with groupwise mean filling of nan values
-    'total_data' : './data/train_gwm_mean_total.parquet',
-    'train_data' : './data/train_gwm_train.parquet',
-    'test_data' : './data/train_gwm_test.parquet',
+    'total_data' : './dataset/train_gwm_mean_total.parquet',
+    'train_data' : './dataset/train_gwm_train.parquet',
+    'test_data' : './dataset/train_gwm_test.parquet',
     #'val_data' : '',
-    'labels_data' : './data/train_labels.csv',
+    'labels_data' : './dataset/train_labels.csv',
     
     # Data with groupwise mean filling of nan values -->> Aggregated based on mean of values
-    'train_data_agg' : './data/train_gwm_train_agg_mean.parquet',
-    'val_data_agg' : './data/train_gwm_test_agg_mean.parquet',
+    'train_data_agg' : './dataset/train_gwm_train_agg_mean.parquet',
+    'val_data_agg' : './dataset/train_gwm_test_agg_mean.parquet',
     #'test_data_agg' : '',
 
     # Data with groupwise mean filling of nan values -->> Aggregated based on mean of values
