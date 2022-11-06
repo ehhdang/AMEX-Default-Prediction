6854aa210266fea24a3e5e3ceb4a8056ff9034cb
amrit <ap4.singh@gmail.com>
2022-11-06 16:30:01 -0500

    preprocess_data_code

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -97,3 +97,15 @@ def get_aggregated_train_test_split():
     test_df_mean = pd.merge(test_df_mean , labels , on = 'customer_ID' , how = 'left')
 
     return train_df_mean ,test_df_mean
+
+
+
+def get_long_train_labels(labels, train_data):
+    """Create a 2D scatterplot of components and labels
+    Args:
+        labels (pd.DataFrame): (N,D) A dataframe containing the customer_ids, with one entry for each customer
+        train_data (pd.DataFrame): (N*12, D) A dataframe containing the customer_ids
+    Returns:
+        labels_long (pd.DataFrame): (N*12,D) A longer version of the training labels where each entry in labels corresponds to the training data.
+    """
+    return train_data[['customer_ID']].merge(labels)
