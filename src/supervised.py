8bb9666c6698ca11130b0bd815fadf7a417f90bc
Hassan Naveed <hassannaveed@live.com>
2022-12-03 17:16:41 -0500

    Added Code for Feedforward NeuralNetworks

--- src/supervised.py
+++ src/supervised.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+
 class supervised_model():
     def __init__(self, model):
         '''
@@ -19,5 +23,23 @@ class supervised_model():
         y_pred_proba = self.model.predict_proba(x_test)[:,1]
         return y_pred , y_pred_proba
 
+class FeedForwardNN(object):
+    def __init__(self,in_dims):
+        super().__init__()
 
+        self.in_dims = in_dims
+
+    def create_net(self):
+        model = Sequential()
+        model.add(tf.keras.Input((self.in_dims)))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+
+        return model
     
+    def compile_net(self, model):
+        self.model = model
+        self.model.compile(loss='binary_crossentropy',
+              optimizer="adam",
+              metrics=[tf.keras.metrics.AUC()])
+        return self.model
