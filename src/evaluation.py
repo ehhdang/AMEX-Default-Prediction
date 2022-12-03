808f7901e1cc050a63d243e0b5fe78e78032580d
Hassan Naveed <hassannaveed@live.com>
2022-12-03 14:08:35 -0500

    identified categorical vars

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,12 +1,16 @@
 
 import pandas as pd
+import numpy as np
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, RocCurveDisplay, roc_curve
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 #!pip install seaborn
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Metrics are available in kaggle post: https://www.kaggle.com/code/rohanrao/amex-competition-metric-implementations
 
 def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 
@@ -59,3 +63,78 @@ def get_results(test_df , gt , preds , pred_prob):
 
     
     return output_dict , pred_df
+
+### TODO: Reconcile this with amex_metric()
+# recreates evaluation metric from AMEX: 
+def evaluate_model(y_true, y_score, traditional = False):
+    # produce traditional metric such as Precision, Recall, F-measure and accuracy
+    if traditional:
+        threshold = 0.5
+        pred_labels  = (y_score[:,1] > 0.5)
+        true_positive = np.sum(np.logical_and(y_true, pred_labels))
+        false_positive = np.sum(np.logical_and(1-y_true, pred_labels))
+        false_negative = np.sum(np.logical_and(y_true, 1-pred_labels))
+        true_negative = np.sum(np.logical_and(1-y_true, 1-pred_labels))
+
+        print("Traditional Metrics:")
+        print(f"    tp: {true_positive}, fp: {false_positive}, tn: {true_negative}, fn: {false_negative}")
+        precision = true_positive / (true_positive + false_positive)
+        print("     The precision score is ", precision)
+        recall = true_positive / (true_positive + false_negative)
+        print("     The recall score is ", recall)
+        accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_negative + false_positive)
+        print("     The accuracy score is ", accuracy)
+        fmeasure = 2/(1/precision + 1/recall)
+        print("     F-Measure: ", fmeasure)
+
+    AUC = roc_auc_score(y_true, y_score[:,1])
+    print(f"AUC Score: {AUC}")
+
+    # GINI from AUC formula: https://yassineelkhal.medium.com/confusion-matrix-auc-and-roc-curve-and-gini-clearly-explained-221788618eb2
+    Gini = (AUC*2)-1
+    print(f"GINI Score: {Gini}")
+
+    #get top 4% ratio of positive predictions
+    fpr, tpr,_ = roc_curve(y_true, y_score[:,1])
+    pred_labels.sort()
+    pos_samples = np.sum(y_true)
+    neg_samples = len(y_true) - pos_samples
+    neg_samples_weighted = 20*neg_samples
+    total_samples = pos_samples+neg_samples_weighted
+    four_percent = int(0.04*total_samples)
+    four_percent_index = np.argmax((fpr * neg_samples_weighted + tpr * pos_samples >= four_percent))
+
+    true_positives_at_four_percent = tpr[four_percent_index]*pos_samples
+
+    D = true_positives_at_four_percent/pos_samples
+    print(f"Default rate at 4%: {D}")
+    
+    M = 0.5*(Gini+D)
+    print(f"M: {M}")
+
+    return M
+
+
+# Plot Confusion Matrix
+def plot_confusion_matrix(y_true, y_score):
+    pred_labels = y_score[:,1]>0.5
+    cm = confusion_matrix(y_true, pred_labels)
+    cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Compliance", "Default"])
+    cm_display.plot()
+    plt.show()
+
+# Plot GINI (OR AUC) curve
+def plot_GINI(y_true, y_score):
+
+    fpr, tpr,_ = roc_curve(y_true, y_score[:,1])
+    pos_samples = np.sum(y_true)
+    neg_samples = len(y_true) - pos_samples
+    neg_samples_weighted = 20*neg_samples
+    total_samples = pos_samples+neg_samples_weighted
+    four_percent = int(0.04*total_samples)
+    four_percent_index = np.argmax((fpr * neg_samples_weighted + tpr * pos_samples >= four_percent))
+
+    RocCurveDisplay.from_predictions(y_true, y_score[:,1])
+    plt.axvline(x = 0.04, color = 'r',linestyle = 'dashed')
+    plt.scatter([fpr[four_percent_index]],[ tpr[four_percent_index]], s = 100)
+    plt.show()
